856ba9442710a6a65b043cfdbb2c19b510c7e2f7
Ashabul Kahfi <105841108523@student.unismuh.ac.id>
2025-09-10 17:41:06 +0800

    add Menu and Navbar components to enhance dashboard layout

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,5 +1,7 @@
 import Link from "next/dist/client/link";
 import Image from "next/image";
+import Menu from "@/components/Menu";
+import Navbar from "@/components/Navbar";
 
 export default function DashboardLayout({
   children,
@@ -8,15 +10,19 @@ export default function DashboardLayout({
 }>) {
     return (
       <div className="h-screen flex">
-        <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] bg-violet-100">
+        <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%]">
           <Link href="/" 
               className="flex items-center justify-center lg:justify-start gap-2 p-4">
           <Image src="/logo.png" alt="Logo" width={32} height={32} />
           <span className="hidden lg:block">Sman 69 Mars</span>
           </Link>
+          <Menu />
           </div>
 
-        <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-slate-200">main content</div>
+        <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-slate-100 overflow-scroll">
+          <Navbar />
+          {children}
+        </div>
         </div>
       );
     }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,28 @@
+import Image from "next/image"
+
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className="flex items-center justify-between p-4">
+      <div className="hidden md:flex">
+        <Image src="/search.png" alt="Search" width={14} height={14} />
+        <input type="text" placeholder="Search..." />
+      </div>
+
+      <div className="flex items-center gap-6">
+        <div className="bg-white rounded-full w-7 h-7 flex items-center justify-center cursor-pointer">
+          <Image src="/message.png" alt="Messages" width={20} height={20} />
+        </div>
+        <div className="bg-white rounded-full w-7 h-7 flex items-center justify-center cursor-pointer relative">
+          <Image src="/announcement.png" alt="Messages" width={20} height={20} />
+          <div className="absolute top-3 right-3 w-5 h-5 flex items-center justify-center bg-purple-300 text-white rounded-full text-xs"></div>
+        </div>
+        <div className="flex flex-col">
+          <span className="text-xs leading-3 font-medium">Kahfi</span>
+          <span className="text-[10px] text-gray-500 text-right">Admin</span>
+        </div>
+        <Image src="/avatar.png" alt="" width={30} height={30} className="rounded-full" />
+      </div>
+    </div>
   )
 }
 
