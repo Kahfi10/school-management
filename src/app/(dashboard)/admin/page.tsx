41bdd6517947f3a62b7c29a6fc4c9134bd8d6286
Ashabul Kahfi <105841108523@student.unismuh.ac.id>
2025-09-14 11:59:21 +0800

    refactor: enhance AdminPage layout and implement UserCard component styling

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -4,7 +4,7 @@ const AdminPage = () => {
   return (
     <div className="p-4 flex gap-4 flex-col md:flex-row">
       <div className="w-full lg:2/3">
-        <div className="flex gap-4 ">
+        <div className="flex gap-4 justify-between flex-wrap">
         <UserCard type="student"/>
         <UserCard type="teacher"/>
         <UserCard type="parent"/>
